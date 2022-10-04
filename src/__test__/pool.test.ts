e6c7a6c54c189ad7c710c32c9c3da57d58d71c4e
Matt Stephens <87805115+mstephen19@users.noreply.github.com>
2022-10-04 18:39:54 +0200

    test(pool)

--- src/__test__/pool.test.ts
+++ src/__test__/pool.test.ts
@@ -1,4 +1,6 @@
 import { pool } from '../pool/index.js';
+import { ConcurrencyOption } from '../types/pool.js';
+import { cpus } from 'os';
 import { api } from './worker.js';
 
 describe('Pool', () => {
@@ -13,5 +15,38 @@ describe('Pool', () => {
             service.close();
             service2.close();
         });
+    });
+
+    it('Should prevent more than the concurrency from running at one time', async () => {
+        pool.setConcurrency(ConcurrencyOption.x1);
+
+        const promise = Promise.all(
+            [...Array(50).keys()].map(() =>
+                (async () => {
+                    await api({ name: 'add', params: [1, 2] });
+                })()
+            )
+        );
+
+        expect(pool.activeCount).toBe(cpus().length);
+
+        await promise;
+    });
+
+    it('Should eventually run all workers queued into it', async () => {
+        pool.setConcurrency(ConcurrencyOption.x1);
+
+        let total = 0;
+
+        await Promise.all(
+            [...Array(50).keys()].map(() =>
+                (async () => {
+                    const value = await api({ name: 'add', params: [1, 0] });
+                    total += value;
+                })()
+            )
+        );
+
+        expect(total).toBe(50);
     });
 });
