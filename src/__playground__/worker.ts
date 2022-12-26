7113e9120682aeae63adb06e0d36ae54b5149801
Matt Stephens <87805115+mstephen19@users.noreply.github.com>
2022-12-26 14:46:31 +0100

    fix

--- src/__playground__/worker.ts
+++ src/__playground__/worker.ts
@@ -1,21 +1,14 @@
-import { define, SharedMap } from '@nanolith';
-import type { SharedMapTransfer } from '@nanolith';
+import { define } from '@nanolith';
 
 export default await define({
-    // Expect to receive a transfer object for a SharedMap with a property
-    // of "foo" which is a string.
-    async myHandler(transfer: SharedMapTransfer<{ foo: string }>) {
-        // Wrap the transfer object in a SharedMap instance so it can be
-        // interacted with.
-        const map = new SharedMap(transfer);
-
-        // Log out the current value of "foo".
-        console.log(await map.get('foo'));
-
-        // Set a new value for "foo". This change can be seen on all threads.
-        await map.set('foo', 'set in the worker');
-
-        // Close the instance, allowing the task to finish.
-        map.close();
+    __beforeTask(context) {
+        console.log('before', context);
+    },
+    __afterTask(context) {
+        console.log('after', context);
+    },
+    async foo() {
+        // await new Promise((r) => setTimeout(r, 10e3));
+        return 'foo';
     },
 });
