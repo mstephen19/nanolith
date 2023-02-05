6f193515d9b3ab0467661ee3851ac2004765fd59
Matt Stephens <87805115+mstephen19@users.noreply.github.com>
2023-02-04 21:00:05 -0800

    refactor(counter)

--- src/utilities/counter.ts
+++ src/utilities/counter.ts
@@ -1,34 +1,20 @@
-import type { Mutex } from '@typing/shared_map.js';
-import { createMutex, lockMutexSync, unlockMutex } from './mutex.js';
+import { createSharedUint32, getValue, setValue } from './shared_uint32_array.js';
+import type { SharedUint32 } from './shared_uint32_array.js';
 
-type Count = Uint32Array;
-
-export type Counter = {
-    lock: Mutex;
-    count: Count;
-};
-
-const createCount = (): Count => new Uint32Array(new SharedArrayBuffer(4));
+export type Counter = SharedUint32;
 
 export const createCounter = (): Counter => {
-    return {
-        lock: createMutex(),
-        count: createCount(),
-    };
+    return createSharedUint32();
 };
 
 export const getCount = (counter: Counter) => {
-    return counter.count[0];
+    return getValue(counter);
 };
 
 export const incr = (counter: Counter) => {
-    lockMutexSync(counter.lock);
-    counter.count[0] = getCount(counter) + 1;
-    unlockMutex(counter.lock);
+    setValue(counter, (num) => num + 1);
 };
 
 export const decr = (counter: Counter) => {
-    lockMutexSync(counter.lock);
-    counter.count[0] = getCount(counter) - 1;
-    unlockMutex(counter.lock);
+    setValue(counter, (num) => num - 1);
 };
